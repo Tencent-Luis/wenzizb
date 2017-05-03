317d26ae1be9896631d35827a784c1f7ce35883e
zhaolu <zhaolu@playcrab.com>
2017-05-03 19:11:58 +0800

    建立socket连接

--- app/controller/login.js
+++ app/controller/login.js
@@ -43,6 +43,46 @@ module.exports = function()
                 _response.render(VIEW + 'main.pug', {'user': value});
             }
         });
+
+        //启动socket服务，监听客户端连接
+        var time = 0;
+        io.sockets.on('connection', function(socket){
+            var user_name = sessionLib.userName;
+            //检查onlineList中，是否有socket连接用户信息
+            if(!onlineList[user_name])
+            {
+                onlineList[user_name] = socket;    //没有,则存储
+            }
+
+            var refresh_online = function(){
+                var n = [];
+                for(var i in onlineList)
+                {
+                    n.push(i);
+                }
+
+                var message = modules.fs.readFileSync(BASE_DIR + '/live_data.txt', 'utf8');   //同步读取文件消息
+                socket.emit('live_data', message);
+                io.sockets.emit('online_list', n);     //对所有人发送消息
+            }
+
+            refresh_online();
+
+            //确保每次只发送一个socket消息
+            if(time > 0)
+            {
+                return;
+            }
+            socket.on('public', function(data){
+                var insertMsg = '<li><span class="icon-user"></span>' + 
+                    '<span class="live_user_name text-success">[owen]</span><span class="live_message text-info">' + 
+                    data.msg + '</span></li>';
+                writeFile({'msg': insertMsg, 'data': data}, function(data){
+                    io.sockets.emit('msg', data);
+                });
+            });
+            
+        });
     }
     
 }
