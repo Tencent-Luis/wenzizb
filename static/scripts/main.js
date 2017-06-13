de8d081fcbff9b5581923127c2a4caad98bf2dcd
zhaolu <zhaolu@playcrab.com>
2017-06-13 11:16:52 +0800

    修改

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -9,6 +9,12 @@ socket.on('online_list', function(user_list){
 	var html = '';
 	for(var i = 0; i < user_list.length; i++)
 	{
+		//非直播页面过滤掉直播员
+		if(user_list[i] == 'owen')
+		{
+			continue;
+		}
+
 		html += '<div class="chat-user"><span class="pull-right label label-success">Online</span>' + 
 		        '<img class="chat-avatar" src="images/a' + (i + 1) + '.jpg" alt="" /><div class="chat-user-name">' + 
 				'<a href="chat_view.html#">' + user_list[i] + '</a></div></div>';					
